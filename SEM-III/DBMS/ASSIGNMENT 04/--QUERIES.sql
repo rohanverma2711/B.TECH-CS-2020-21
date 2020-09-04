f8e1ea9188c645253703a8ec2b13f40febc2b57c
Rohan Kumar <rohan.kumar_cs19@gla.ac.in>
2020-09-04 20:03:52 +0530

    OS ASSIGNMENT

--- SEM-III/DBMS/ASSIGNMENT 04/--QUERIES.sql
+++ SEM-III/DBMS/ASSIGNMENT 04/--QUERIES.sql
@@ -31,3 +31,43 @@ SELECT AVG(DISTINCT(GPA)) FROM STUDENT;
 
 --QUERY 10
 SELECT SID,CNAME,MAJOR,DECISION  FROM APPLY WHERE DECISION = 'Y';
+
+--Q11. Find number of students having GPA>3.4 and coming from high school having size>1000.
+
+SELECT COUNT(sID) FROM STUDENT WHERE GPA >3.4 AND sizeHS >1000;
+
+
+--Q12. Find how many student applied to ‘marine biology’.
+
+SELECT COUNT(sID) APPLIED_STUDENTS
+FROM APPLY 
+WHERE major = 'marine biology';
+
+
+--Q13. Find how many applications were rejected and accepted by the colleges.
+
+SELECT COUNT(SID) APPLIED_STUDENTS
+FROM APPLY 
+WHERE DECISION = 'Y';
+SELECT COUNT(SID) REJECTED_STUDENTS
+FROM APPLY 
+WHERE DECISION = 'N';
+
+--Q14. Find how many students applied to a particular major. (show count(sid) as No_of_applications).
+SELECT MAJOR,COUNT(SID) NO_OF_APPLICATIONS
+FROM APPLY
+GROUP BY MAJOR;
+
+
+--Q15. Find number of applications received by particular college
+
+SELECT cNAME,COUNT(SID) NO_OF_APPLICATIONS
+FROM APPLY
+GROUP BY cNAME;
+
+--Q16. Find number of applications received in a particular major at a particular college.
+
+SELECT cNAME,MAJOR,COUNT(SID) NO_OF_APPLICATIONS
+FROM APPLY
+GROUP BY cNAME,MAJOR;
+
